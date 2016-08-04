8703930eb3df88067731e46f3e09bf22c85d0484
rogerbf <roger@bergfalk.nu>
2016-08-04 08:54:29 +0200

    parsing experiments

--- src/index.js
+++ src/index.js
@@ -6,20 +6,53 @@ dump()
   .then(d => {
     const docs = d.split(divider).slice(1)
     console.log(docs.length)
+    // console.log(docs[0])
+    // console.log(
+    //   docs
+    //     .map(doc => {
+    //       return (
+    //       doc
+    //         .trim()
+    //         .split('\n')
+    //         .map(line => {
+    //           return (
+    //           line
+    //             .split('\t')
+    //             .join(' ')
+    //             .trim()
+    //           )
+    //         })
+    //         // .reduce((acc, line) => {
+    //         //   const key = line.slice(0, line.indexOf(':'))
+    //         //   const value = line.slice(line.indexOf(':') + 1).trim()
+    //         //   const pair = {}
+    //         //   pair[key] = value
+    //         //   return Object.assign({}, acc, pair)
+    //         // }, {})
+    //       )
+    //     })[0]
+    // )
     console.log(
-      docs.map(
-        doc => {
-          doc
-            .trim()
-            .split('\n')
-            .map(l => {
-              return l.split('\t').join(' ').trim()
-            })
-            .filter(l => {
-              return l.split(':')[0] === 'bundle id'
-            })
-        }
-      ).length
+      docs
+        .map(doc => {
+          return doc
+            .split('\n\t')
+            .map(l => l.trim())
+            .reduce((acc, curr, i) => {
+              if (i === 0 && curr.indexOf('\nbundle\tid:')) {
+                return acc
+                  .concat(curr.slice(0, curr.indexOf('\n')))
+                  .concat(curr.slice(curr.indexOf('\n') + 1).replace('\t', ' '))
+              }
+              if (curr[0] !== '<') {
+                return acc.concat(Array.of(curr))
+              } else {
+                const narr = [].concat(acc)
+                narr[narr.length - 1] += curr
+                return narr
+              }
+            }, [])
+        })[0]
     )
   })
   .catch(e => console.error(e))
