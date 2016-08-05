d059b8d5c3603c2cf2d1f15f46dbf64b6d453a51
rogerbf <roger@bergfalk.nu>
2016-08-05 23:38:58 +0200

    parsing into objects

--- src/index.js
+++ src/index.js
@@ -1,39 +1,13 @@
 const dump = require('./lib/dump.js')
 
 const divider = Array(80).fill('-').join('')
+const wierd = Array(55).fill('-').join('')
 
 dump()
-  .then(d => {
-    const docs = d.split(divider).slice(1)
-    console.log(docs.length)
-    // console.log(docs[0])
-    // console.log(
-    //   docs
-    //     .map(doc => {
-    //       return (
-    //       doc
-    //         .trim()
-    //         .split('\n')
-    //         .map(line => {
-    //           return (
-    //           line
-    //             .split('\t')
-    //             .join(' ')
-    //             .trim()
-    //           )
-    //         })
-    //         // .reduce((acc, line) => {
-    //         //   const key = line.slice(0, line.indexOf(':'))
-    //         //   const value = line.slice(line.indexOf(':') + 1).trim()
-    //         //   const pair = {}
-    //         //   pair[key] = value
-    //         //   return Object.assign({}, acc, pair)
-    //         // }, {})
-    //       )
-    //     })[0]
-    // )
-    console.log(
-      docs
+  .then(data => {
+    const docs = data.split(divider).slice(1)
+    // console.log(docs.length)
+      const results = docs
         .map(doc => {
           return doc
             .split('\n\t')
@@ -47,6 +21,9 @@ dump()
               if (curr.indexOf('displayVersion') === 0) {
                 return acc.concat(Array.of(curr.replace('displayVersion', 'displayVersion:')))
               }
+              if (curr.indexOf(wierd) === 0) {
+                return acc
+              }
               if (curr[0] !== '<') {
                 return acc.concat(Array.of(curr))
               } else {
@@ -55,7 +32,70 @@ dump()
                 return narr
               }
             }, [])
-        })[0]
-    )
+        })
+        // Simple and not very effective test to see if every line in every doc contains a semicolon, indicating parsable data.
+        // .reduce((semicolons, doc) => {
+        //   const withsemicolon = doc.reduce((acc, curr) => {
+        //     if (curr.indexOf(':')) {
+        //       return acc + 1
+        //     } else {
+        //       return acc
+        //     }
+        //   }, 0)
+        //   if (doc.length === withsemicolon) {
+        //     return semicolons + 1
+        //   } else {
+        //     return semicolons
+        //   }
+        // }, 0)
+
+        // Experimenting...
+        // .reduce((jsdocs, doc) => {
+        //   return jsdocs.concat([
+        //     docs.reduce((obj, line) => {
+        //       const key = line.slice(0, line.indexOf(':')).trim()
+        //       const value = line.slice(line.indexOf(':') + 1).trim()
+        //       const o = {}
+        //       o[key] = value
+        //       return Object.assign({}, obj, o)
+        //     }, {})
+        //   ])
+        // }, [])
+        // console.log(JSON.stringify(results, null, 2))
+
+        // experimenting...
+        // const ra = results.slice(100, 200)
+        // const ro = ra.reduce((acc, doc) => {
+        //   return acc.concat([
+        //     doc.reduce((o, l) => {
+        //       const k = l
+        //         .slice(0, l.indexOf(':'))
+        //         .replace(/\s./g, match => match[1].toUpperCase())
+        //         .replace(/-/g, match => '_')
+        //       const v = l.slice(l.indexOf(':') + 1).trim()
+        //       const no = {}
+        //       no[k] = v
+        //       return Object.assign(o, no)
+        //     }, {})
+        //   ])
+        // }, [])
+        // process.stdout.write(JSON.stringify(ro, null, 2))
+
+        .reduce((acc, doc) => {
+          return acc.concat([
+            doc.reduce((o, l) => {
+              const k = l
+                .slice(0, l.indexOf(':'))
+                .replace(/\s./g, match => match[1].toUpperCase())
+                .replace(/-/g, match => '_')
+              const v = l.slice(l.indexOf(':') + 1).trim()
+              const no = {}
+              no[k] = v
+              return Object.assign(o, no)
+            }, {})
+          ])
+        }, [])
+
+        process.stdout.write(JSON.stringify(results, null, 2))
   })
   .catch(e => console.error(e))
